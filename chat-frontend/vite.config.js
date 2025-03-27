fcaee7f8c380e61dc2d18233b2262919d5cf64d1
Arnavjain2503 <166912655+Arnavjain2503@users.noreply.github.com>
2025-03-27 15:54:39 +0530

    Added docker files

--- chat-frontend/vite.config.js
+++ chat-frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// FOR DEPLOYMENT
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',  // Allow access from external IPs
+    port: 5000,        // Run on port 5000
+    strictPort: true,  // Prevent switching to another port if 5000 is busy
+  },
+});

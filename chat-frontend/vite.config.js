8f4708bdffd649568c9fc3df1a2c69e81772cd6d
Abhishek-2502 <abhishek25022004@gmail.com>
2025-04-06 13:14:58 +0530

    Modifying for deploy

--- chat-frontend/vite.config.js
+++ chat-frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // since you're serving from root
+  build: {
+    outDir: 'dist', // output folder
+    sourcemap: false,
+  },
+});
